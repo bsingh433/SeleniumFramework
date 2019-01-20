df209b18cd3abe680b0a41a981ed5215a34977b7
Shivangi <Shivangi@Shivangi>
2019-01-20 16:15:38 +0530

    First Commit

---
+++ src/main/java/com/driver/WebDriverFactory.java
@@ -0,0 +1,10 @@
+package com.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverFactory {
+	public static WebDriver getWebDriver() {
+		return new ChromeDriver();
+	}
+}
